c40f91a374823926b346960aa5b25d88afd4f755
bfagalde <bfagalde@uy.ibm.com>
2017-04-20 11:54:20 -0300

    Update Main1.java

--- UnProyectoJava/src/unproyectojava/Main1.java
+++ UnProyectoJava/src/unproyectojava/Main1.java
@@ -4,7 +4,7 @@ public final class Main1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("version 4");
+		System.out.println("cambiando a version 4.1");
 	}
 
 }
