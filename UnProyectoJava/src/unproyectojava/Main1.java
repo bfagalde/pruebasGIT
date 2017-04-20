1d15d92d7a6b873bf40c541aba017038f8d2da5d
bfagalde <bfagalde@IBM577-PC0G082F>
2017-04-20 11:38:23 -0300

    subiendo archivo

---
+++ UnProyectoJava/src/unproyectojava/Main1.java
@@ -0,0 +1,10 @@
+package unproyectojava;
+
+public final class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("version 3");
+	}
+
+}
