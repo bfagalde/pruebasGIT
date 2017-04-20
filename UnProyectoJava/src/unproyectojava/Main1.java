9ee59f2bbd3b8939b5896334e64f1d6f47b82e4f
bfagalde <bfagalde@IBM577-PC0G082F>
2017-04-20 11:41:59 -0300

    moficiando

--- UnProyectoJava/src/unproyectojava/Main1.java
+++ UnProyectoJava/src/unproyectojava/Main1.java
@@ -4,7 +4,7 @@ public final class Main1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("version 3");
+		System.out.println("version 4");
 	}
 
 }
